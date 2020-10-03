5df2960b73fb9331982bfd4514ffb6414a7223d9
chris shino <chris.shino@hotmail.com>
2020-10-03 12:41:29 -0400

    added 3

--- workshop/_fundamentals/__tests__/exercise-3.test.js
+++ workshop/_fundamentals/__tests__/exercise-3.test.js
@@ -4,4 +4,7 @@ const getPairing = require("../exercise-3");
 test("Exercise 3", () => {
   expect(getPairing("burger")).toBe("beer");
   // add more tests here...
+  expect(getPairing(1)).toBe(`I didn't get that!`);
+  expect(getPairing("fish")).toBe("white wine");
+  expect(getPairing("beef")).toBe("red wine");
 });
