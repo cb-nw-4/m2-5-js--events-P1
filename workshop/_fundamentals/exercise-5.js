7d19e0b71c58ed12abbfb52a623ae7edb494c90d
Vanessa Chan <chw.vanessa@gmail.com>
2020-10-03 15:57:36 -0400

    Fundamentals - 5

--- workshop/_fundamentals/exercise-5.js
+++ workshop/_fundamentals/exercise-5.js
@@ -8,6 +8,26 @@
 
 const getDaysInMonth = (month) => {
   // Insert missing solution please
+
+  let daysOfMonth = {
+    January : 31,
+    February: 28,
+    March: 31,
+    April: 30,
+    May: 31,
+    June: 30,
+    July: 31,
+    August: 31,
+    September: 30,
+    October: 31,
+    November: 30,
+    December:31
+  }
+
+  if(daysOfMonth[month]==undefined){
+    return "error";
+  }
+  return daysOfMonth[month];
 };
 
 // 2. Do a console.log to verify your function.
