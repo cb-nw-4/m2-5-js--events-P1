23abe07d47df9d1666a38f4c8aac0fa0691f13d8
Doug Wright <dw@nof49.ca>
2020-10-03 15:59:04 -0400

    Completed fundamentals 5

--- workshop/_fundamentals/__tests__/exercise-5.test.js
+++ workshop/_fundamentals/__tests__/exercise-5.test.js
@@ -3,5 +3,8 @@ const getDaysInMonth = require("../exercise-5");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 5", () => {
   expect(getDaysInMonth("November")).toBe(30);
-  // add more tests here...
+  expect(getDaysInMonth("October")).toBe(31);
+  expect(getDaysInMonth("February")).toBe(28);
+  expect(getDaysInMonth("April")).toBe(30);
+  expect(getDaysInMonth("Thirteenber")).toBe('error');
 });
