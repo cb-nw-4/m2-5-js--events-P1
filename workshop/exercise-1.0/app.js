b07364bac32531b3f85dea34b9288bcef5cf5f85
Bharat Katyal <bharatkatyal@live.com>
2020-10-03 13:02:48 -0400

    1.1 complete

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,8 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+document.addEventListener("click", function(){
+  document.getElementById("main").innerHTML = "Hello World";
+
+  });
--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,25 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+
+// const timeOut = document.body.addEventListener("click", 1000);
+// console.log(timeOut) // Some how test is this shows true of false
+
+let userLost = false;
+
+setTimeout(function(){
+userLost= true;
+}, 1000)
+
+
+document.addEventListener("click", function(){
+  if (userLost){
+    document.getElementById("result").innerHTML = "You Lost";
+
+  } else {
+    document.getElementById("result").innerHTML = "You Won";
+
+  }
+
+  });
+
