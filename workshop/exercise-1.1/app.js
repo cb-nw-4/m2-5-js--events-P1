bab6b31820e45728fa2b3ae8403c484fa12e40ae
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-03 16:30:11 -0400

    Exercise-1.1 answered

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,12 @@
 // Exercise 1.1
-// ------------
+const para = document.querySelector('.result');
+const container = document.querySelector('body');
+container.addEventListener('click', handleClick);
+
+function handleClick() {
+    para.innerText = 'You Win!';
+    para.style.color = 'red'; //not required by the exercise
+    para.style.fontSize = '50px'; //not required by the exercise
+};
+
 console.log('exercise 1.1');
