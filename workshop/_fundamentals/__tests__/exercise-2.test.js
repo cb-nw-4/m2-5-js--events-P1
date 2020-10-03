ea193aa3353c431ce87543de43075c4009e06c3d
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-03 14:32:37 -0400

    Exercise-2 answered

--- workshop/_fundamentals/__tests__/exercise-2.test.js
+++ workshop/_fundamentals/__tests__/exercise-2.test.js
@@ -6,4 +6,13 @@ test("Exercise 2", () => {
     combineArrays(["crisp", "bacon"], ["zuchini", "apple"])
   ).toStrictEqual(["apple", "bacon", "crisp", "zuchini"]);
   // add more tests here...
+  expect(
+    combineArrays(["January", "February"], ["March", "April", "May", "June"])
+  ).toStrictEqual(["April", "February", "January", "June", "March", "May" ]);
+  expect(
+    combineArrays([false, "February"], ["March", "April", "May", "June"])
+  ).toStrictEqual(["April", "February", "June", "March", "May", false ]);
+  expect(
+    combineArrays([9, "February"], ["March", "April", "May", "June"])
+  ).toStrictEqual([9, "April", "February", "June", "March", "May"]);
 });
--- workshop/_fundamentals/exercise-2.js
+++ workshop/_fundamentals/exercise-2.js
@@ -6,10 +6,18 @@
 // - if any of the values in either array is not strings, return undefined.
 
 const combineArrays = (arr1, arr2) => {
-  // Insert missing solution please
+  let arr3 = arr1.concat(arr2);
+  let strAlphabetical;
+  for (let i = 0; i < arr3.length; i++) {
+    if (typeof arr3[i] === "string") {
+      strAlphabetical = arr3.sort();
+    }
+  }
+  return (strAlphabetical);
 };
 
 // 2. Do a console.log to verify your function.
+console.log(combineArrays(["crisp", "bacon"], ['zuchini', 'apple']));
 
 // 3. Test your function.
 // Look for the corresponding exercise file in the __tests__ folder.
