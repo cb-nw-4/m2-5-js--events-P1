c19e77b416d42c4ce64f1c35c20b2d71989da9b7
“Boris <your@email.comBoris>
2020-10-03 16:41:13 -0400

    Finished Exercise 1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,24 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+document.addEventListener("click", win);
+let winner = false;
+
+function win(){
+    document.querySelector(".result").innerHTML = "You Won!";
+    document.removeEventListener("click", lose);
+    winner = true;
+}
+
+setTimeout(() => {
+    document.removeEventListener("click", win);
+    if (winner === false) document.addEventListener("click", lose); 
+}, 1000);
+
+
+function lose(){
+    document.querySelector(".result").innerHTML = "Too Slow!";
+    winner = false;
+}
+
+
