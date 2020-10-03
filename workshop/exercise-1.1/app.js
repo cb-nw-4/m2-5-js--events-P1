eee8f8884ab9a999f2c2ce1c9ba047c3e760f772
chris shino <chris.shino@hotmail.com>
2020-10-03 14:16:18 -0400

    finished 1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,32 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+console.log("exercise 1.1");
+
+const getBody = document.querySelector("body");
+const getPara = document.querySelector("p");
+let timer = true;
+
+// window.addEventListener("load", startTimer);
+// getBody.addEventListener("click", clickMe);
+
+const clickMe = () => {
+  if (timer === true) {
+    return (getPara.innerText = "You Won!");
+  } else {
+    return (getPara.innerText = "You lose!");
+  }
+};
+
+const startTimer = () => {
+  setTimeout(() => {
+    timer = false;
+    console.log(timer);
+  }, 1000);
+
+};
+
+window.addEventListener("load", startTimer);
+getBody.addEventListener("click", clickMe);
+
+
+console.log(timer);
