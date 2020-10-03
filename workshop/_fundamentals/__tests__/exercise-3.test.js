1ae9e8b809cfebe7ed655d634e67aa24ec43c660
Doug Wright <dw@nof49.ca>
2020-10-03 15:15:08 -0400

    Completed fundamentals 3

--- workshop/_fundamentals/__tests__/exercise-3.test.js
+++ workshop/_fundamentals/__tests__/exercise-3.test.js
@@ -3,5 +3,8 @@ const getPairing = require("../exercise-3");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 3", () => {
   expect(getPairing("burger")).toBe("beer");
-  // add more tests here...
+  expect(getPairing("fish")).toBe("white wine");
+  expect(getPairing("beef")).toBe("red wine");
+  expect(getPairing("strawberries")).toBe("champagne");
+  expect(getPairing(1)).toBe("I didn't get that!");
 });
