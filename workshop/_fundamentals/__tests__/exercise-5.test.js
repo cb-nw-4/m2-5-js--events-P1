ba4e7b12cfa900263a8474f17c7ea822d1f454f2
chris shino <chris.shino@hotmail.com>
2020-10-03 13:11:03 -0400

    done 5

--- workshop/_fundamentals/__tests__/exercise-5.test.js
+++ workshop/_fundamentals/__tests__/exercise-5.test.js
@@ -3,5 +3,8 @@ const getDaysInMonth = require("../exercise-5");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 5", () => {
   expect(getDaysInMonth("November")).toBe(30);
+  expect(getDaysInMonth("January")).toBe(31);
+  expect(getDaysInMonth("February")).toBe(28);
+  expect(getDaysInMonth(1)).toBe(undefined);
   // add more tests here...
 });
