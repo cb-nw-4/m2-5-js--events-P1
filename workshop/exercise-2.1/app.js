f6ce6a8fc49666902c893b7dfb34b8cc9b6095d3
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-10-04 12:41:44 -0400

    ex2.1

--- workshop/exercise-2.1/app.js
+++ workshop/exercise-2.1/app.js
@@ -1,6 +1,8 @@
 console.log("exercise 2.1");
 
 let area=document.querySelector(".main");
+area.style.margin="5px 100px 0 100px";
+//area.style.display="flex";
 
 for(let i=1;i<=20;i++){
     let btnarr=document.createElement('button');
@@ -8,6 +10,11 @@ for(let i=1;i<=20;i++){
     btnarr.innerText=i;
     area.appendChild(btnarr);
     btnarr.addEventListener('click', newcolor);
+    btnarr.style.color="white";
+    btnarr.style.fontSize="18px";
+    btnarr.style.backgroundColor="Red";
+    btnarr.style.height="120px";
+    btnarr.style.width="120px";
 }
 
 function newcolor(event){
