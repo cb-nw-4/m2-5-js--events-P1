313ba28de286df34836c84b1309dcbc5c65ee442
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-10-04 12:26:45 -0400

    ex1.2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,7 +1,12 @@
 // Exercise 1.2
 // ------------
 console.log('exercise 1.2');
-
+document.body.style.backgroundImage='radial-gradient(purple,blue)';
+let styling=document.querySelector(".time-text");
+styling.style.fontFamily='Comic Neue, cursive';
+styling.style.fontSize="40px";
+styling.style.color="white";
+styling.style.padding="20px";
 
 let range=Math.floor(Math.random() * 11);  
 console.log("Range="+range);
@@ -10,14 +15,22 @@ let counter=document.getElementById("time");
 counter.innerText=range;
 let counter2=setInterval(() => {
     range=range-1;
-    counter.innerText=`${range}`;
-    if(range<0){
+    if(range>=0){
+        counter.innerText=`${range}`;
+    }
+    else{
         clearInterval(counter2);
     }
 }, 1000);
 
 let result=document.querySelector("#result");
 document.body.addEventListener('click', display);
+result.style.fontSize="150px";
+result.style.fontFamily='Comic Neue, cursive';
+result.style.color="yellow";
+result.style.position="absolute";
+result.style.left="25%";
+result.style.top="25%";
 
 let event1=setTimeout(function(){
     result.innerText="You lose!";
