831db48946ea271812651a803a9689a061baa867
Doug Wright <dw@nof49.ca>
2020-10-05 11:12:39 -0400

    Completed exercise 1.2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,25 @@
 // Exercise 1.2
 // ------------
 console.log('exercise 1.2');
+
+let timeRemaining = Math.floor((Math.random() * 5) + 1);
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+h1.innerHTML = 'You have <span style="color:yellow;">' + timeRemaining + '</span> seconds to click';
+
+function handleMouseClick() {
+    clearInterval(timer);
+    document.querySelector('.result').innerText = 'You Win!';
+}
+
+body.addEventListener('click', handleMouseClick);
+
+const timer = setInterval(function() {
+    timeRemaining--;
+    h1.innerHTML = 'You have <span style="color:yellow;">' + timeRemaining + '</span> seconds to click';
+    if (timeRemaining === 0) {
+        clearInterval(timer);
+        document.querySelector('.result').innerText = 'You Lose!';
+        body.removeEventListener('click', handleMouseClick);
+    }
+}, 1000);
