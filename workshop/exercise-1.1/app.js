939bcdd591273a6728f7a2923a95c18f8c8b448d
Doug Wright <dw@nof49.ca>
2020-10-05 10:39:56 -0400

    Completed exercise 1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,21 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+
+let timeOut = false;
+const body = document.querySelector('body');
+
+function handleMouseClick() {
+    clearInterval(timer);
+    if (timeOut) {
+        document.querySelector('.result').innerText = 'You Lose!';
+    } else {
+        document.querySelector('.result').innerText = 'You Win!';
+    }
+}
+
+body.addEventListener('click', handleMouseClick);
+
+const timer = setInterval(function() {
+    timeOut = true;
+}, 1000);
