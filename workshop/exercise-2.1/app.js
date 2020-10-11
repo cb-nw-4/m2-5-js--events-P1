2796600361994c8322ec3a29b633ab8022a735a5
Alexandre <alex_18h@hotmail.com>
2020-10-11 17:10:48 -0400

    exercise 2.1

--- workshop/exercise-2.1/app.js
+++ workshop/exercise-2.1/app.js
@@ -1,1 +1,15 @@
-const main = document.querySelector("main")
+const main = document.querySelector(".main")
+const body = document.querySelector("body")
+
+for (let i = 1; i < 21; i++) {
+    main.appendChild(document.createElement("button")).innerText = `${i}`
+}
+
+const redButton = document.querySelectorAll("button")
+console.log(redButton)
+redButton.forEach(button => {
+    button.addEventListener('click' , () => {
+        button.style = 'background-color: darkgreen'
+    })
+    console.log(button)
+})
