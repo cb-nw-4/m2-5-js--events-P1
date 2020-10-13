df4821daf3566d655026c30e877df320f7b961e5
“Winnie <chanwinnie.wc@gmail.com>
2020-10-12 21:39:45 -0400

    Exercise 1.0 completed

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,12 @@
 // Exercise 1.0
 // ------------
+const div = document.querySelector("#main");
+
+function popUp() {
+  document.getElementById("main");
+  div.innerHTML = "You Win!";
+}
+
+document.addEventListener("click", popUp);
+
 console.log('exercise-1');
