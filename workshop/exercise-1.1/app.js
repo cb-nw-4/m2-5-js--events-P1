8fe33a8922ada21c01d27a70abef5da312c92539
“Winnie <chanwinnie.wc@gmail.com>
2020-10-12 21:40:06 -0400

    Exercise 1.1 completed

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,37 @@
 // Exercise 1.1
 // ------------
+const body = document.querySelector("body");
+body.style.height = "100vh";
+body.style.background = "radial-gradient(#000033, #000099, deepskyblue)";
+
+const p = document.querySelector("#result");
+
+const quickClick = document.createElement("h1");
+quickClick.innerHTML = "Be a quicker clicker";
+quickClick.style.color = "white";
+quickClick.style.fontSize = "30px";
+body.appendChild(quickClick);
+body.insertBefore(quickClick, p);
+console.log(quickClick);
+
+let clicked = false;
+document.addEventListener("click", clickedFunc);
+function clickedFunc(e){
+    clicked = true;
+}
+
+setTimeout(clickedOrNot, 1000);
+
+function clickedOrNot() {
+    if (clicked === true) {
+        p.innerHTML = "You Win!"
+    } else {
+        p.innerHTML = "You Lose!"
+    }
+}
+
+p.style.color = "magenta";
+p.style.fontSize = "155px";
+p.style.textAlign = "center";
+
 console.log('exercise 1.1');
