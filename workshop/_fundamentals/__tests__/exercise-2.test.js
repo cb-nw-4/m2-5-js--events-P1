89eead6c7ea060cc25dc9a9b07aeb39d4605995b
“Winnie <chanwinnie.wc@gmail.com>
2020-10-12 21:38:00 -0400

    Exercise-2 completed

--- workshop/_fundamentals/__tests__/exercise-2.test.js
+++ workshop/_fundamentals/__tests__/exercise-2.test.js
@@ -6,4 +6,13 @@ test("Exercise 2", () => {
     combineArrays(["crisp", "bacon"], ["zuchini", "apple"])
   ).toStrictEqual(["apple", "bacon", "crisp", "zuchini"]);
   // add more tests here...
+  expect(
+    combineArrays(["a", "b", "t"], ["z", "f"])
+  ).toStrictEqual(["a", "b", "f", "t", "z"]);
+  expect(
+    combineArrays(["Dec", "Jan"], [true, "March"])
+  ).toStrictEqual(undefined); 
+  expect(
+    combineArrays([1, 2, 3], [4, 5, 6])
+  ).toStrictEqual(undefined);
 });
